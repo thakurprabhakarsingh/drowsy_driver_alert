d50b10dbe095661c18bfae2b20f65d1e2c9fb3d8
Prabhakar Singh <prabhakarsingh8586@gmail.com>
2026-09-17 09:55:01 +0530

    Set minSdk to 21 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.drowsy_driver_alert"
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
